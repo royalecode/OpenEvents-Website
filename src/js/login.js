24af5d8f18d4ad7c2e90a3b87f3c18bd9fc83a8a
Edmon Bosch <edmonbosch@gmail.com>
2021-04-14 22:57:22 +0200

    login finsihed

--- src/js/login.js
+++ src/js/login.js
@@ -2,33 +2,33 @@ window.addEventListener('load', loginLoaded);
 
 function loginLoaded() {
     console.log('Page loaded');
-    //const form = document.getElementsByName('login-form')
-    const data = {
-        email: document.getElementById('email').value,
-        password: document.getElementById('pwd').value
-    };
-
+    
     let token;
-
     var loginButton = document.getElementById('login-button');
     loginButton.addEventListener('click', () => {
 
-    fetch("http://puigmal.salle.url.edu/api/users/login", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(data),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data.accessToken);
-        token = data.accessToken;
-    })
-    .catch(ex => {
-        console.log("error al post login")
-    });
-    window.open("../html/listevent.html");
-    //window.location.replace("../html/listevent.html");
+        const data = {
+            email: document.getElementById('email').value,
+            password: document.getElementById('pwd').value
+        };
+
+        fetch("http://puigmal.salle.url.edu/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data.accessToken);
+            token = data.accessToken;
+        })
+        .catch(ex => {
+            console.log(ex);
+        });
+        
+        //window.open("../html/listevent.html");
+        //window.location.replace("../html/listevent.html");
     });
 }
--- src/js/register.js
+++ src/js/register.js
@@ -1,24 +1,33 @@
 window.addEventListener('load', pageLoaded);
 
 function registerUser(event) {
+    
     console.log("hola");
-    //let imageFile = getBase64Image("https://sites.google.com/site/paris20151905/_/rsrc/1472871434925/imatges/1216_paris-en-otono.jpg");
-    const user = {
-        image: "https://sites.google.com/site/paris20151905/_/rsrc/1472871434925/imatges/1216_paris-en-otono.jpg",
-        name: document.getElementById('fname').value,
-        last_name: document.getElementById('lname').value,
-        email: document.getElementById('email').value,
-        password: document.getElementById('pwd').value,
-    };
-    console.log(user);
-    console.log(JSON.stringify(user));
-
+    // const user = {
+    //     image: document.getElementById('file').value,
+    //     name: document.getElementById('fname').value,
+    //     last_name: document.getElementById('lname').value,
+    //     email: document.getElementById('email').value,
+    //     password: document.getElementById('pwd').value,
+    // };
+    // console.log(user);
+    //formElement = document.getElementById('register-form');
+    var formData = new FormData();
+    formData.append('image', document.getElementById('file').files[0]);
+    formdata.append('name', document.getElementById('fname').value);
+    formData.append('last_name', document.getElementById('lname').value);
+    formData.append('email', document.getElementById('email').value);
+    formData.append('password', document.getElementById('pwd').value);
+    console.log(...formData);
+    //console.log(JSON.stringify(user));
+    
     fetch("http://puigmal.salle.url.edu/api/users ", {
         method: "POST",
         headers: {
-            "Content-type": "application/json",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify(user),
+        body: formData,
     })
     .then(function(response) {
         console.log(response.status); // Will show you the status
@@ -35,7 +44,7 @@ function registerUser(event) {
     });
 
     event.preventDefault();
-    window.open("../html/login.html");
+    //window.open("../html/login.html");
 }
 
 function pageLoaded() {
