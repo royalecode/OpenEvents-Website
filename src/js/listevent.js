b9eb249849b72b1e188eb21a2ea702e72ed2dc80
Edmon Bosch <edmonbosch@gmail.com>
2021-05-13 00:14:57 +0200

    add logout to other modules, and fetch add assistance

--- src/js/listevent.js
+++ src/js/listevent.js
@@ -1,40 +1,99 @@
+import {logoutUser, security } from './basicFunctionalities.js';
+security();
 window.addEventListener('load', listPageLoad);
+let logoutIcon = document.getElementById('logoutIcon');
+var app;
 
 function listPageLoad() {
+    logoutIcon.addEventListener('click', logoutCallback);
 
-    var app = new Vue({
+    app = new Vue({
         el: '#app',
         data: {
-            events: [
-
-            ]
+            events: []
         },
         mounted(){
-
-            fetch(`http://puigmal.salle.url.edu/api/events`, {
-                method: "GET"
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                if(data.length == 0){
-                    console.log("No hi ha events a la plataforma");
-                }else{
-                    console.log(data);
-                    data.map((e) => {
-                        e.image = "http://puigmal.salle.url.edu/img/" + e.image;
-                        //console.log(e.image);
-                        e.eventStart_date = e.eventStart_date.split("T")[0];
-                        e.eventStart_date = e.eventStart_date.replaceAll("-","/");
-                        e.eventEnd_date = e.eventEnd_date.split("T")[0];
-                        e.eventEnd_date = e.eventEnd_date.replaceAll("-","/");
-                        return e;
-                      })
-                    this.events = data;
-                }
-            })
-            .catch(ex => {
-                console.log(ex);
-            });  
+            loadEvents();
+        },
+        methods: {
+            participate: function (event_id){
+                console.log(event_id);
+                addParticipation(event_id);
+            }
         }
     })
+}
+
+function loadEvents(){
+    fetch(`http://puigmal.salle.url.edu/api/events/`, {
+        method: "GET"
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if(data.length == 0){
+            console.log("No hi ha events a la plataforma");
+        }else{
+            console.log(data);
+            data.map((e) => {
+                e.image = "http://puigmal.salle.url.edu/img/" + e.image;
+                //console.log(e.image);
+                e.eventStart_date = e.eventStart_date.split("T")[0];
+                e.eventStart_date = e.eventStart_date.replaceAll("-","/");
+                e.eventEnd_date = e.eventEnd_date.split("T")[0];
+                e.eventEnd_date = e.eventEnd_date.replaceAll("-","/");
+                return e;
+              })
+            app.events = data;
+        }
+    })
+    .catch(ex => {
+        console.log(ex);
+    });
+}
+
+function logoutCallback() {
+    console.log("Logout icon clicked");
+    logoutUser();
+}
+
+function addParticipation(event_id) {
+    var token = localStorage.getItem('token');
+
+    fetch(`http://puigmal.salle.url.edu/api/events/${event_id}/assistances`, {
+        method: "POST",
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then((response) => {
+        if (!response.ok) {
+            response.json().then((error) => {
+                console.log(error);
+            });
+        } else {
+            console.log("Participació afegida correctament");
+        }
+    })
+    .catch(ex => {
+        console.log(ex);
+    });
+
+    fetch(`http://puigmal.salle.url.edu/api/events/2/assistances`, {
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if(data.length == 0){
+            console.log("No hi assistents a l'event");
+        }else{
+            console.log(data);
+            
+        }
+    })
+    .catch(ex => {
+        console.log(ex);
+    });
 }
--- src/js/search.js
+++ src/js/search.js
@@ -1,81 +1,94 @@
+import {logoutUser, security } from './basicFunctionalities.js';
+security();
 window.addEventListener('load', listSearchPageLoad);
+let logoutIcon = document.getElementById('logoutIcon');
 let input;
 var app;
 
 function listSearchPageLoad() {
+    logoutIcon.addEventListener('click', logoutCallback);
     input = document.getElementById('search');
     input.addEventListener('keypress', renderEvents);
 
     app = new Vue({
         el: '#appSearch',
         data: {
-            events: [
-    
-            ]
+            events: []
         },
         mounted(){
-    
-            fetch(`http://puigmal.salle.url.edu/api/events/`, {
-                method: "GET"
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                if(data.length == 0){
-                    console.log("No hi ha events a la plataforma");
-                }else{
-                    console.log(data);
-                    data.map((e) => {
-                        e.image = "http://puigmal.salle.url.edu/img/" + e.image;
-                        //console.log(e.image);
-                        e.eventStart_date = e.eventStart_date.split("T")[0];
-                        e.eventStart_date = e.eventStart_date.replaceAll("-","/");
-                        e.eventEnd_date = e.eventEnd_date.split("T")[0];
-                        e.eventEnd_date = e.eventEnd_date.replaceAll("-","/");
-                        return e;
-                      })
-                    this.events = data;
-                }
-            })
-            .catch(ex => {
-                console.log(ex);
-            });  
+            loadEvents();
         },
         methods: {
             update: function (){
-                fetch(`http://puigmal.salle.url.edu/api/events/${input.value}`, {
-                    method: "GET"
-                })
-                .then((res) => res.json())
-                .then((data) => {
-                    if(data.length == 0){
-                        console.log("No hi ha events a la plataforma");
-                    }else{
-                        console.log(data);
-                        data.map((e) => {
-                            e.image = "http://puigmal.salle.url.edu/img/" + e.image;
-                            //console.log(e.image);
-                            e.eventStart_date = e.eventStart_date.split("T")[0];
-                            e.eventStart_date = e.eventStart_date.replaceAll("-","/");
-                            e.eventEnd_date = e.eventEnd_date.split("T")[0];
-                            e.eventEnd_date = e.eventEnd_date.replaceAll("-","/");
-                            return e;
-                          })
-                        this.events.length = 0;
-                        this.events = data;
-                    }
-                })
-                .catch(ex => {
-                    console.log(ex);
-                });  
+                loadEventsFilterType();
             }
-            
         }
     })
-    console.log(input.value);
 }
 
 function renderEvents(e){
     if (e.key === 'Enter'){
         app.update();
+        input.value = "";
     }
+}
+
+function loadEvents(){
+    fetch(`http://puigmal.salle.url.edu/api/events/`, {
+        method: "GET"
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if(data.length == 0){
+            console.log("No hi ha events a la plataforma");
+        }else{
+            console.log(data);
+            data.map((e) => {
+                e.image = "http://puigmal.salle.url.edu/img/" + e.image;
+                //console.log(e.image);
+                e.eventStart_date = e.eventStart_date.split("T")[0];
+                e.eventStart_date = e.eventStart_date.replaceAll("-","/");
+                e.eventEnd_date = e.eventEnd_date.split("T")[0];
+                e.eventEnd_date = e.eventEnd_date.replaceAll("-","/");
+                return e;
+              })
+            app.events = data;
+        }
+    })
+    .catch(ex => {
+        console.log(ex);
+    });
+}
+
+function loadEventsFilterType(){
+    fetch(`http://puigmal.salle.url.edu/api/events/${input.value}`, {
+        method: "GET"
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if(data.length == 0){
+            console.log("No hi ha events a la plataforma");
+        }else{
+            console.log(data);
+            data.map((e) => {
+                e.image = "http://puigmal.salle.url.edu/img/" + e.image;
+                //console.log(e.image);
+                e.eventStart_date = e.eventStart_date.split("T")[0];
+                e.eventStart_date = e.eventStart_date.replaceAll("-","/");
+                e.eventEnd_date = e.eventEnd_date.split("T")[0];
+                e.eventEnd_date = e.eventEnd_date.replaceAll("-","/");
+                return e;
+              })
+            app.events.length = 0;
+            app.events = data;
+        }
+    })
+    .catch(ex => {
+        console.log(ex);
+    });  
+}
+
+function logoutCallback() {
+    console.log("Logout icon clicked");
+    logoutUser();
 }
